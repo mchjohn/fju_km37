37ee9b0ded6b87aabff3c8137ec4ee9fd6a5ae9f
Michel John <michel.john@hotmail.com>
2023-02-21 09:58:34 -0300

    refactor: carousel and infoCard

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -7,61 +7,75 @@ import {
   useDisclose,
 } from 'native-base';
 
+import { Skeleton } from '../Skeleton';
 import { ModalInfo } from '../ModalInfo';
+import { Transition } from '../Transition';
 
 type InfoCardProps = {
   url: string;
   title: string;
+  isLoading: boolean;
   description: string;
 };
 
-export function InfoCard({ url, title, description }: InfoCardProps) {
+export function InfoCard({
+  url,
+  title,
+  isLoading,
+  description,
+}: InfoCardProps) {
   const { isOpen, onOpen, onClose } = useDisclose();
 
   return (
     <>
-      <Button
-        p={0}
-        mt={2}
-        onPress={onOpen}
-        variant="unstyled"
-        alignItems="center"
-      >
-        <Box
-          w="100%"
-          rounded="lg"
-          overflow="hidden"
-          borderWidth="1"
-          borderColor="gray.100"
-        >
-          <Box>
-            <AspectRatio w="100%" ratio={16 / 9}>
-              <Image
-                source={{
-                  uri: url,
-                }}
-                resizeMode="stretch"
-                alt="Tema do encontro"
-              />
-            </AspectRatio>
-            <Center
-              px="3"
-              py="1.5"
-              bg="secondary.500"
-              bottom="0"
-              position="absolute"
-              roundedTopRight={4}
-              _text={{
-                fontSize: 'xs',
-                color: 'gray.50',
-                fontWeight: '700',
-              }}
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        <Transition>
+          <Button
+            p={0}
+            mt={2}
+            onPress={onOpen}
+            variant="unstyled"
+            alignItems="center"
+          >
+            <Box
+              w="100%"
+              rounded="lg"
+              overflow="hidden"
+              borderWidth="1"
+              borderColor="gray.100"
             >
-              {title}
-            </Center>
-          </Box>
-        </Box>
-      </Button>
+              <Box>
+                <AspectRatio w="100%" ratio={16 / 9}>
+                  <Image
+                    source={{
+                      uri: url,
+                    }}
+                    resizeMode="stretch"
+                    alt="Tema do encontro"
+                  />
+                </AspectRatio>
+                <Center
+                  px="3"
+                  py="1.5"
+                  bg="secondary.500"
+                  bottom="0"
+                  position="absolute"
+                  roundedTopRight={4}
+                  _text={{
+                    fontSize: 'xs',
+                    color: 'gray.50',
+                    fontWeight: '700',
+                  }}
+                >
+                  {title}
+                </Center>
+              </Box>
+            </Box>
+          </Button>
+        </Transition>
+      )}
 
       <ModalInfo
         title={title}
