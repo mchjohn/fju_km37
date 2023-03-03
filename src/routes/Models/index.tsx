3bc7b6684360ceb33f39ac753ea5240cc556b0e9
Michel John <michel.john@hotmail.com>
2023-03-02 22:32:12 -0300

    refactor: change screen name games to DuelOfTribes

--- src/routes/Models/index.tsx
+++ src/routes/Models/index.tsx
@@ -2,8 +2,8 @@ import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 export type PropsNavigationBottom = {
   Home: undefined;
-  Games: undefined;
   ChurchInfo: undefined;
+  DuelOfTribes: undefined;
   YouthEncounter: undefined;
 };
 
