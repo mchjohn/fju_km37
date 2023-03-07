154d4184f4e71b987f78beb8183b42e8bf5ace4e
Michel John <michel.john@hotmail.com>
2023-03-07 19:37:29 -0300

    refactor: video player screen

--- src/screens/VideoPlayer/index.tsx
+++ src/screens/VideoPlayer/index.tsx
@@ -1,13 +1,17 @@
-import { Box } from 'native-base';
-import { useNavigation } from '@react-navigation/native';
+import { Dimensions } from 'react-native';
 import { useCallback, useState } from 'react';
 import YoutubePlayer from 'react-native-youtube-iframe';
+import { useNavigation } from '@react-navigation/native';
+import { Center, VStack, Heading, Spinner, ZStack } from 'native-base';
 
 import { PropsScreenStack } from '@routes/Models';
 
+const width = Dimensions.get('window').width;
+
 export function VideoPlayer({ route }: PropsScreenStack) {
   const { navigate } = useNavigation();
-  const [playing, setPlaying] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [playing, setPlaying] = useState(true);
 
   const onStateChange = useCallback(
     (state: string) => {
@@ -21,13 +25,30 @@ export function VideoPlayer({ route }: PropsScreenStack) {
   );
 
   return (
-    <Box>
-      <YoutubePlayer
-        play={playing}
-        height={300}
-        videoId={route.params.id}
-        onChangeState={onStateChange}
-      />
-    </Box>
+    <Center flex={1} bg="primary.800">
+      <ZStack alignItems="center" justifyContent="center">
+        <YoutubePlayer
+          play={playing}
+          width={width}
+          height={ready ? 400 : 0}
+          videoId={route.params.id}
+          onChangeState={onStateChange}
+          onReady={() => setReady(true)}
+        />
+
+        {!ready && (
+          <VStack space={2} justifyContent="center">
+            <Spinner
+              size="lg"
+              color="red.600"
+              accessibilityLabel="Carregando vídeo"
+            />
+            <Heading color="red.600" fontSize="md">
+              Calma jovem, estamos carregando o vídeo...
+            </Heading>
+          </VStack>
+        )}
+      </ZStack>
+    </Center>
   );
 }
